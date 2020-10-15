b347c0d6a78972f488b3db627184cf2aeac6663a
thehungryd3v <th3hungryd3v@gmail.com>
2020-10-14 17:17:56 -0700

    test push

---
+++ src/components/OrderComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+class OrderComponent extends Component {
+    render() {
+        return (
+            <div>
+                Order Component!
+                <div>
+                    <Link to={'/home'}>Home</Link>
+                    <Link to={'/home'}>Home</Link>
+                </div>
+            </div>
+        )
+    }
+}
+export default OrderComponent;
